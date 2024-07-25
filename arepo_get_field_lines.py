in code units | fix.py to test and resolve issues with pocker_finder

arepo_get_field_lines.py
@@ -1,69 +1,70 @@
+from collections import defaultdict
+from multiprocessing import Pool
+import random
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
-from scipy.spatial import distance
 from scipy import spatial
 import healpy as hp
 import numpy as np
-import time
-import matplotlib
 import random
-import copy
+import time
 import h5py
-import os
+import json
 import sys
 
 from library import *
 
-FloatType = np.float64
-IntType = np.int32
+"""  
+Using Margo Data
+
+Analysis of reduction factor
+
+$$N(s) 1 - \sqrt{1-B(s)/B_l}$$
+
+Where $B_l$ corresponds with (in region of randomly choosen point) the lowest between the highest peak at both left and right.
+where $s$ is a random chosen point at original 128x128x128 grid.
+
+1.- Randomly select a point in the 3D Grid. 
+2.- Follow field lines until finding B_l, if non-existent then change point.
+3.- Repeat 10k times
+4.- Plot into a histogram.
+
+contain results using at least 20 boxes that contain equally spaced intervals for the reduction factor.
+
+# Calculating Histogram for Reduction Factor in Randomized Positions in the 128**3 Cube 
 
 """
-Start of the code 
-
-python3 colors.py [zoom_boundary] [time-snap]
-
-- [zoom_boundary] default is undefined
-	will perform test with several values to 
-- [time-snap] default is 0.0
-
-Volume of a Voronoi Cell
-
-SurfaceArea 	AREA 	UnitLength2 a**2 h**−2 Surface area of a Voronoi cell (OUTPUT_SURFACE_AREA)
-NumFacesCell 	NFAC 	Adimensonal            Number of faces of a Voronoi cell (OUTPUT_SURFACE_AREA)
-
-Volume = ?
-
-python3 arepo_field_lines.py [Number of points] [rloc_boundary] [rloc_center]
-
-"""
-	
 
 """
 Parameters
 
 - [N] default is 50 as the total number of steps in the simulation
-- [dx] default 4/N of the zoom_boundary (radius of spherical region of interest) variable
+- [dx] default 4/N of the rloc_boundary (radius of spherical region of interest) variable
 
 """
 FloatType = np.float64
 IntType = np.int32
 
 if len(sys.argv)>2:
-	# first argument is a number related to zoom_boundary
+	# first argument is a number related to rloc_boundary
 	N=int(sys.argv[1])
-	zoom_boundary=float(sys.argv[2])
-	zoom_center=float(sys.argv[3])
+	rloc_boundary=float(sys.argv[2])
+	rloc_center  =int(sys.argv[3])
+	max_cycles   =int(sys.argv[4])
 else:
-	N            =100
-	zoom_boundary=80   # rloc_boundary for boundary region of the cloud
-	zoom_center  =1      # rloc_boundary for inner region of the cloud
+    N            =100
+    rloc_boundary=256   # rloc_boundary for boundary region of the cloud
+    rloc_center  =1     # rloc_boundary for inner region of the cloud
+    max_cycles   =100
 
-pp = sys.argv[-1]
+
+# flow control to repeat calculations in no peak situations
+cycle = 0 
+
+reduction_factor_at_gas_density = defaultdict()
+
+reduction_factor = np.array([])
 
 filename = 'arepo_data/snap_430.hdf5'
-
-
-start_time=time.time()
 
 """
 Functions/Methods
@@ -140,12 +141,11 @@ Density_grad = np.zeros((len(Density),3))
 Mass = np.array(data['PartType0']['Masses'], dtype=FloatType)
 Volume = Mass/Density
 
-
 # printing relevant info about the data
 
-Bfield  *= 1# 1/1.496e8 * (1.496e13/1.9885e33)**(-1/2) # in cgs
-Density *= 1#1.9885e33 * (1.496e13)**(-3)				# in cgs
-Mass    *= 1#1.9885e33
+Bfield  *= 1.0#1/1.496e8 * (1.496e13/1.9885e33)**(-1/2) # in cgs
+Density *= 1.0#1.9885e33 * (1.496e13)**(-3)				# in cgs
+Mass    *= 1.0#1.9885e33
 Volume   = Mass/Density
 
 #Center= 0.5 * Boxsize * np.ones(3) # Center
@@ -159,125 +159,157 @@ Center = Pos[np.argmax(Density),:] #430
 VoronoiPos-=Center
 Pos-=Center
 
+VoronoiPos *= 1.0#1.496e13
+Pos        *= 1.0#1.496e13
+
 xPosFromCenter = Pos[:,0]
 Pos[xPosFromCenter > Boxsize/2,0]       -= Boxsize
 VoronoiPos[xPosFromCenter > Boxsize/2,0] -= Boxsize
 
-# idk how but this picks the regions to be worked on	
-nside = 1      # sets number of cells sampling the spherical boundary layers = 12*nside**2
-npix  = 1 #12 * nside ** 2 
-
-rloc_boundary  = zoom_boundary      # radius of the boundary in cgs units. (zoom_boundary is of the order or less of the Boxsize)
-rloc_center    = zoom_center		# radius of sphere at the center of cloud (order of 1% of zoom_boundary)
-
-# Add BOLA (slicing the spherical region we want to work with)
-
-ipix_center       = np.arange(npix)
-#print(ipix_center)
-
-xx,yy,zz = hp.pixelfunc.pix2vec(nside, ipix_center)
-#print(xx)
-
 print("Steps in Simulation: ", N)
-print("rloc_boundary      : ", zoom_boundary)
-print("rloc_center        : ", zoom_center)
+print("rloc_boundary      : ", rloc_boundary)
+print("rloc_center        : ", rloc_center)
+print("max_cycles         : ", max_cycles)
 print("\nBoxsize: ", Boxsize) # 256
 print("Center: ", Center) # 256
 print("Position of Max Density: ", Pos[np.argmax(Density),:]) # 256
 print("Smallest Volume: ", Volume[np.argmin(Volume)]) # 256
 print("Biggest  Volume: ", Volume[np.argmax(Volume)],"\n") # 256
 
-if True:
-	nside = 8     # sets number of cells sampling the spherical boundary layers = 12*nside**2
-	npix  = 12 * nside ** 2 
-	ipix_center       = np.arange(npix)
-	xx,yy,zz = hp.pixelfunc.pix2vec(nside, ipix_center)
-	xx = np.array(random.sample(sorted(xx),1))
-	yy = np.array(random.sample(sorted(yy),1))
-	zz = np.array(random.sample(sorted(zz),1))
+def get_along_lines(x_init):
 
-m = len(zz) # amount of values that hold which_up_down
+    line      = np.zeros((N+1,m,3)) # from N+1 elements to the double, since it propagates forward and backward
+    bfields   = np.zeros((N+1,m))
+    densities = np.zeros((N+1,m))
 
-x_init = np.zeros((m,3))
-x_init[:,0]      = rloc_center * xx[:]
-x_init[:,1]      = rloc_center * yy[:]
-x_init[:,2]      = rloc_center * zz[:]
+    line_rev=np.zeros((N+1,m,3)) # from N+1 elements to the double, since it propagates forward and backward
+    bfields_rev = np.zeros((N+1,m))
+    densities_rev = np.zeros((N+1,m))
 
-line      = np.zeros((N+1,m,3)) # from N+1 elements to the double, since it propagates forward and backward
-bfields   = np.zeros((N+1,m))
-densities = np.zeros((N+1,m))
+    line[0,:,:]     =x_init
+    line_rev[0,:,:] =x_init
 
-line_rev=np.zeros((N+1,m,3)) # from N+1 elements to the double, since it propagates forward and backward
-bfields_rev = np.zeros((N+1,m))
-densities_rev = np.zeros((N+1,m))
+    x = x_init
 
-line[0,:,:]     =x_init
-line_rev[0,:,:] =x_init
+    print(x_init[0,:])
 
-x = x_init
+    dummy, bfields[0,:], densities[0,:], cells = find_points_and_get_fields(x, Bfield, Density, Density_grad, Pos)
+    dummy, bfields_rev[0,:], densities_rev[0,:], cells = find_points_and_get_fields(x, Bfield, Density, Density_grad, Pos)
 
-print(x_init[0,:])
+    # propagates from same inner region to the outside in -dx direction
+    for k in range(N):
+        
+        dx = 1.0
+        x, bfield, dens = Heun_step(x, dx, Bfield, Density, Density_grad, VoronoiPos)
+        
+        line[k+1,:,:] = x
+        bfields[k+1,:] = bfield
+        densities[k+1,:] = dens
 
-dummy, bfields[0,:], densities[0,:], cells = find_points_and_get_fields(x, Bfield, Density, Density_grad, Pos)
-dummy, bfields_rev[0,:], densities_rev[0,:], cells = find_points_and_get_fields(x, Bfield, Density, Density_grad, Pos)
+    # propagates from same inner region to the outside in -dx direction
 
-# propagates from same inner region to the outside in -dx direction
-for k in range(N):
-	
-	dx = 1.0
-	x, bfield, dens = Heun_step(x, dx, Bfield, Density, Density_grad, VoronoiPos)
-	
-	line[k+1,:,:] = x
-	bfields[k+1,:] = bfield
-	densities[k+1,:] = dens
+    for k in range(N):
+        x, bfield, dens = Heun_step(x, -dx, Bfield, Density, Density_grad, VoronoiPos)
+        
+        line_rev[k+1,:,:] = x
+        bfields_rev[k+1,:] = bfield
+        densities_rev[k+1,:] = dens
 
-# propagates from same inner region to the outside in -dx direction
+    line_rev = line_rev[1:,:,:]
+    bfields_rev = bfields_rev[1:,:] 
+    densities_rev = densities_rev[1:,:]
 
-for k in range(N):
-	x, bfield, dens = Heun_step(x, -dx, Bfield, Density, Density_grad, VoronoiPos)
-	
-	line_rev[k+1,:,:] = x
-	bfields_rev[k+1,:] = bfield
-	densities_rev[k+1,:] = dens
+    dens_min = np.log10(min(np.min(densities),np.min(densities_rev)))
+    dens_max = np.log10(max(np.max(densities),np.max(densities_rev)))
 
-line_rev = line_rev[1:,:,:]
-bfields_rev = bfields_rev[1:,:] 
-densities_rev = densities_rev[1:,:]
+    dens_diff = dens_max - dens_min
 
-dens_min = np.log10(min(np.min(densities),np.min(densities_rev)))
-dens_max = np.log10(max(np.max(densities),np.max(densities_rev)))
+    path           = np.append(line, line_rev, axis=0)
+    path_bfields   = np.append(bfields, bfields_rev, axis=0)
+    path_densities = np.append(densities, densities_rev, axis=0)
 
-dens_diff = dens_max - dens_min
+    for j, _ in enumerate(path[0,:,0]):
+        # for trajectory 
+        radius_vector      = np.zeros_like(path[:,j,:])
+        magnetic_fields    = np.zeros_like(path_bfields[:,j])
+        gas_densities      = np.zeros_like(path_densities[:,j])
+        trajectory         = np.zeros_like(path[:,j,0])
 
-path           = np.append(line, line_rev, axis=0)
-path_bfields   = np.append(bfields, bfields_rev, axis=0)
-path_densities = np.append(densities, densities_rev, axis=0)
+        prev_radius_vector = path[0,j,:]
+        diff_rj_ri = 0.0
 
-for j, _ in enumerate(path[0,:,0]):
-	# for trajectory 
-	radius_vector      = np.zeros_like(path[:,j,:])
-	magnetic_fields    = np.zeros_like(path_bfields[:,j])
-	gas_densities      = np.zeros_like(path_densities[:,j])
-	trajectory         = np.zeros_like(path[:,j,0])
+        for k, pk in enumerate(path[:,j,0]):
+            
+            radius_vector[k]    = path[k,j,:]
+            magnetic_fields[k]  = path_bfields[k,j]
+            gas_densities[k]    = path_densities[k,j]
+            diff_rj_ri = magnitude(radius_vector[k], prev_radius_vector)
+            trajectory[k] = trajectory[k-1] + diff_rj_ri
+            #print(radius_vector[k], magnetic_fields[k], gas_densities[k], diff_rj_ri)
+            
+            prev_radius_vector  = radius_vector[k] 
 
-	prev_radius_vector = path[0,j,:]
-	diff_rj_ri = 0.0
+        trajectory[0] *= 0.0
 
-	for k, pk in enumerate(path[:,j,0]):
-		
-		radius_vector[k]    = path[k,j,:]
-		magnetic_fields[k]  = path_bfields[k,j]
-		gas_densities[k]    = path_densities[k,j]
-		diff_rj_ri = magnitude(radius_vector[k], prev_radius_vector)
-		trajectory[k] = trajectory[k-1] + diff_rj_ri
-		#print(radius_vector[k], magnetic_fields[k], gas_densities[k], diff_rj_ri)
-		
-		prev_radius_vector  = radius_vector[k] 
+    return radius_vector, trajectory, magnetic_fields, gas_densities
 
-	trajectory[0] *= 0.0
+if sys.argv[-1] == "-1":
+    # Assuming your JSON file is named 'data.json'
+    file_path = 'random_distributed_reduction_factor.json'
 
-time_end = time.time()-start_time
-elapsed_time_minutes= time_end/60
+    # Open the file in read mode
+    with open(file_path, 'r') as file:
+        # Load the JSON data into a Python list
+        reduction_factor = np.array(json.load(file))
+
+    cycle == int(max_cycles)
+
+# Generate a list of tasks
+tasks = []
+for i in range(1):
+    
+    rloc_center      = float(random.uniform(0,1)*float(rloc_boundary)/4)
+    
+    if True:
+        nside = 8     # sets number of cells sampling the spherical boundary layers = 12*nside**2
+        npix  = 12 * nside ** 2 
+        ipix_center       = np.arange(npix)
+        xx,yy,zz = hp.pixelfunc.pix2vec(nside, ipix_center)
+        xx = np.array(random.sample(sorted(xx),1))
+        yy = np.array(random.sample(sorted(yy),1))
+        zz = np.array(random.sample(sorted(zz),1))
+
+    m = len(zz) # amount of values that hold which_up_down
+
+    x_init = np.zeros((m,3))
+    x_init[:,0]      = rloc_center * xx[:]
+    x_init[:,1]      = rloc_center * yy[:]
+    x_init[:,2]      = rloc_center * zz[:]
+
+    initial_conditions = (x_init)
+    print("rloc_center:= ", rloc_center, list(x_init[0]))
+    tasks.append((initial_conditions))
+        
+
+#distance, bfield, numb_density = 
+# Number of worker processes
+num_workers = 6
+
+# Record the start time
+start_time = time.time()
+
+# Create a Pool of worker processes
+with Pool(num_workers) as pool:
+    # Map the euler_integration function to the list of tasks
+    results = pool.map(get_along_lines, tasks)
+
+# Record the end time
+elapsed_time = time.time() - start_time
+
+# Print elapsed time
+print(f"Elapsed time: {elapsed_time/60} Minutes")
+
+radius_vector, trajectory, magnetic_fields, gas_densities = results[0]
 
 np.save("arepo_output_data/ArePositions.npy", radius_vector)
 np.save("arepo_output_data/ArepoTrajectory.npy", trajectory)
@@ -316,13 +348,13 @@ if True:
 
 	ax = plt.figure().add_subplot(projection='3d')
 
-	for k in range(m):
+	for k in range(1):
 		print(k)
-		x=path[:,k,0] # 1D array
-		y=path[:,k,1]
-		z=path[:,k,2]
+		x=trajectory[:,k,0] # 1D array
+		y=trajectory[:,k,1]
+		z=trajectory[:,k,2]
 		
-		which = x**2 + y**2 + z**2 <= zoom_boundary**2
+		which = x**2 + y**2 + z**2 <= rloc_boundary**2
 		
 		x=x[which]
 		y=y[which]
@@ -336,9 +368,9 @@ if True:
 		ax.scatter(x[-1], y[-1], z[-1], marker="x", color="r",s=6)
 		
 
-	ax.set_xlim(-zoom_boundary,zoom_boundary)
-	ax.set_ylim(-zoom_boundary,zoom_boundary)
-	ax.set_zlim(-zoom_boundary,zoom_boundary)
+	ax.set_xlim(-rloc_boundary,rloc_boundary)
+	ax.set_ylim(-rloc_boundary,rloc_boundary)
+	ax.set_zlim(-rloc_boundary,rloc_boundary)
 	ax.set_xlabel('x [AU]')
 	ax.set_ylabel('y [AU]')
 	ax.set_zlabel('z [AU]')
@@ -347,6 +379,4 @@ if True:
 	#plt.savefig(f'field_shapes/MagneticFieldThreading.png',bbox_inches='tight')
 
 	#plt.close()
-	#plt.show()
-
-print("Elapsed Time (Minutes): ", elapsed_time_minutes)
+	plt.show()