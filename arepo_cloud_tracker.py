c7e7fb2108be54023bcd6c44c0eb0e7e8f07c178
ByronEncinas <byron_encinas@outlook.com>
2024-12-11 16:55:51 -0700

    cloudtracker test 22 | CloudVel sign

--- arepo_cloud_tracker.py
+++ arepo_cloud_tracker.py
@@ -273,7 +273,7 @@ for fileno, filename in enumerate(file_list[::-1]):
     xc = Pos[:, 0]
     yc = Pos[:, 1]
     zc = Pos[:, 2]
-    region_radius = 10
+    region_radius = 20
     print(Pos[np.argmax(Density),:])
     print(Velocities.shape)
 
@@ -291,7 +291,7 @@ for fileno, filename in enumerate(file_list[::-1]):
 
         delta_time_seconds = abs(time_value-prev_time) * seconds_in_myr
 
-        UpdatedCord = CloudCord +  CloudVelocity * km_to_parsec * delta_time_seconds
+        UpdatedCord = CloudCord - CloudVelocity * km_to_parsec * delta_time_seconds
 
         #region_radius = 0.5*np.linalg.norm(CloudVelocity) * time_code_units
 
