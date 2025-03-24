b1a6c39397015ba4b10251878ca1c9caa5525d4d
ByronEncinas <byron_encinas@outlook.com>
2025-03-23 19:58:12 -0600

    test: arepo dynamic cloud size

--- arepo_cloud_tracker.py
+++ arepo_cloud_tracker.py
@@ -73,22 +73,23 @@ for list in file_list:
         print(children_folder)
         os.makedirs(children_folder, exist_ok=True)
         Boxsize = data['Header'].attrs['BoxSize']
+
         VoronoiPos = np.asarray(data['PartType0']['Coordinates'], dtype=FloatType)
         Pos = np.asarray(data['PartType0']['CenterOfMass'], dtype=FloatType)
-        Bfield = np.asarray(data['PartType0']['MagneticField'], dtype=FloatType)
         Density = np.asarray(data['PartType0']['Density'], dtype=FloatType)
         Mass = np.asarray(data['PartType0']['Masses'], dtype=FloatType)
         Velocities = np.asarray(data['PartType0']['Velocities'], dtype=FloatType)
         Momentums = Mass[:, np.newaxis] * Velocities
-        Bfield_grad = np.zeros((len(Pos), 9))
-        Density_grad = np.zeros((len(Density), 3))
+
         Volume   = Mass/Density
         time_code_units = time_value*myrs_to_code_units
         delta_time_seconds = (time_value-prev_time)*seconds_in_myr
+        
         xc = Pos[:, 0]
         yc = Pos[:, 1]
         zc = Pos[:, 2]
-        region_radius = 1
+
+        region_radius = 0.1
             
         if fileno == 0:
             CloudCord = Pos[np.argmax(Density), :]
@@ -98,42 +99,56 @@ for list in file_list:
                 file.write(f"{snap},{time_value},{CloudCord[0]},{CloudCord[1]},{CloudCord[2]},0.0,0.0,0.0,{PeakDensity}\n")
         else:
             cloud_sphere = ((xc - CloudCord[0])**2 + (yc - CloudCord[1])**2 + (zc - CloudCord[2])**2 < region_radius**2)
-            
+            Radius = np.sqrt((xc - CloudCord[0])**2 + (yc - CloudCord[1])**2 + (zc - CloudCord[2])**2)
+
             if np.any(cloud_sphere):
                 CloudCord = Pos[cloud_sphere][np.argmax(Density[cloud_sphere]), :]
             else:
                 print(f"Warning: No particles found within region_radius of {region_radius} around CloudCord.")
             
-            CloudVelocity = np.sum(Momentums[cloud_sphere], axis=0) / np.sum(Mass[cloud_sphere])
-
-            delta_time_seconds = abs(time_value - prev_time) * seconds_in_myr
-            UpdatedCord = CloudCord - CloudVelocity * km_to_parsec * delta_time_seconds
+            #CloudVelocity = np.sum(Momentums[cloud_sphere], axis=0) / np.sum(Mass[cloud_sphere])
+            #delta_time_seconds = abs(time_value - prev_time) * seconds_in_myr
+            #UpdatedCord = CloudCord - CloudVelocity * km_to_parsec * delta_time_seconds
             if np.any(cloud_sphere):
                 CloudCord = Pos[cloud_sphere][np.argmax(Density[cloud_sphere]), :]
                 PeakDensity = Density[cloud_sphere][np.argmax(Density[cloud_sphere])]*gr_cm3_to_nuclei_cm3
             else:
                 print(f"Warning: No particles found within updated region_radius of {region_radius} around UpdatedCord.")
             with open(f"cloud_tracker_slices/{typpe}/{typpe}_cloud_trajectory.txt", "a") as file:
-                file.write(f"{snap},{time_value},{CloudCord[0]},{CloudCord[1]},{CloudCord[2]},{CloudVelocity[0]},{CloudVelocity[1]},{CloudVelocity[2]},{PeakDensity}\n")
+                file.write(f"{snap},{time_value},{CloudCord[0]},{CloudCord[1]},{CloudCord[2]},0.0,0.0,0.0,{PeakDensity}\n")
 
         prev_time = time_value
+
+        import seaborn as sns
+
+        cloud_sphere = ((xc - CloudCord[0])**2 + (yc - CloudCord[1])**2 + (zc - CloudCord[2])**2 < region_radius**2)
+        Radius = np.sqrt((xc - CloudCord[0])**2 + (yc - CloudCord[1])**2 + (zc - CloudCord[2])**2)[cloud_sphere]
+        SphereDensity = (Density[cloud_sphere]*gr_cm3_to_nuclei_cm3)
+        mask  = (SphereDensity > 100)
+        radius_core = Radius[mask]
+        print(len(radius_core)/len(cloud_sphere), np.mean(radius_core))
+        region_radius = np.max(radius_core)
+
         ds = yt.load(filename)
-        ad = ds.all_data()
+
         sp = yt.SlicePlot(
             ds, 
             'z', 
             ('gas', 'density'), 
             center=[CloudCord[0], CloudCord[1], CloudCord[2]],
-            width = region_radius
+            width=region_radius*1.2
         )
-        sp.annotate_marker(
-            [CloudCord[0], CloudCord[1], CloudCord[2]],
-            marker='x',
-            color='red',
-            s=100
+
+        sp.annotate_sphere(
+            [CloudCord[0], CloudCord[1], CloudCord[2]],  # Center of the sphere
+            radius=(region_radius*0.5, "pc"),  # Radius of the sphere (in physical units, e.g., pc)
+            circle_args={"color": "black", "linewidth": 2}  # Styling for the sphere
         )
         sp.annotate_timestamp(redshift=False)
         sp.annotate_scale()
-        sp.save(os.path.join(parent_folder, f"{typpe}_{snap}_slice_z.png"))
 
+        sp.save(os.path.join(parent_folder, f"{typpe}_{snap}_slice_z.png"))
+        
+        region_radius = np.max(radius_core)*1.2
+        
         continue
