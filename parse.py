0e8fed24458958f6fe05fcd091deb8f36bfc5959
ByronEncinas <byron_encinas@outlook.com>
2025-06-26 14:30:43 -0600

    grid size to gs=22

--- parse.py
+++ parse.py
@@ -554,7 +554,7 @@ if False:
     plt.savefig("./amb_l_p.png")
     plt.close()
 
-gs = 25
+gs = 22
 times, r100 = zip(*[(float(time), r100_distro[r100_distro < 1])
                     for time, _, r100_distro, *rest in ReducedBundle['ideal']])
 
